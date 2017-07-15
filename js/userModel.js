71e19228a3558d850b5f30046d7061c9cc8de13a
Adam James <adjam1212@gmail.com>
2017-07-15 15:11:59 +0100

    Update userModel.js

--- js/userModel.js
+++ js/userModel.js
@@ -1,75 +1,3 @@
-﻿//jQuery slider for phong shininess level
-$("#shine").slider({
-    orientation: "horizontal",
-    min: 10,
-    max: 500,
-    value: 10,
-    slide: function (event, ui) {
-        materials.phongMaterial.shininess = ui.value; //Set shininess parameter to the current selected value of slider
-    }
-});
-
-$("#shine").slider({
-    change: function (event, ui) {
-        console.log(ui.value);
-        materials.phongMaterial.shininess = ui.value; //Set shininess of phong material to value from the slider
-    }
-});
-
-$(".glow_select").spectrum({
-    color: "#fff",
-    change: function (color) {
-        $("#basic-log").text("Hex Colour Selected: " + color.toHexString()); //Log information
-        glow_value = $(".glow_select").spectrum('get').toHexString(); //Get the colour selected
-        //Set glow material colour to selected value
-        materials.glowMaterial.uniforms.glowColor.type = "c";
-        materials.glowMaterial.uniforms.glowColor.value = new THREE.Color(glow_value);
-    }
-});
-
-
-
-var gridHelper;
-$('#grid').change(function () {
-    if (grid.checked) {
-        gridHelper.visible = true;
-    }
-    else {
-        gridHelper.visible = false;
-    }
-});
-
-var polar_grid_helper;
-$('#polar_grid').change(function () {
-    if (polar_grid.checked) {
-        polar_grid_helper.visible = true;
-    }
-    else {
-        polar_grid_helper.visible = false;
-    }
-});
-
-var axis_view;
-$('#axis').change(function () {
-    if (axis.checked) {
-        axis_view.visible = true;
-    }
-    else {
-        axis_view.visible = false;
-    }
-});
-
-
-var bound_box;
-$('#bBox').change(function () {
-    if (bBox.checked) {
-        bound_box.visible = true;
-    }
-    else {
-        bound_box.visible = false;
-    }
-});
-
 /*READ FILE*/
 document.getElementById('obj_file').addEventListener('change', readFile, false); //Read .obj file
 
@@ -116,73 +44,24 @@ function readFile(evt) {
                                 + 'Number of faces: ' + geometry.faces.length;
                         }
 
-
-                        child.material = materials.default_material;
-
-                        var geo = new THREE.EdgesGeometry(child.geometry); // or WireframeGeometry
-                        var mat = new THREE.LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
-                        var wireframe = new THREE.LineSegments(geo, mat);
+                        child.material = materials.default_material;            
 
                         setWireFrame(child);
+                        setWireframeAndModel(model);
                         setGlowModel(model);
                         setGlow(child);
-
-                        setWireFrame(child);
                         setPhong(child);
-                        setXray(child);
-
-
-                        $('#model_wire').change(function () {
-                            if (model_wire.checked) {
-                                wire.checked = false;
-                                child.material = materials.default_material;
-                                child.add(wireframe);
-                            }
-                            else {
-                                child.material = materials.default_material;
-                                child.remove(wireframe);
-                            }
-                        });
+                        setXray(child);                      
 
                     }
                 });
 
                 model.position.set(0, 0, 0);
 
-                bound_box = new THREE.BoxHelper(model); //, 0xffffff
-                bound_box.visible = false;
-                model.add(bound_box); //Add bounding box helper to model (for when checkbox is checked)
-
-                var bbox = new THREE.Box3().setFromObject(model);
-                console.log(bbox.min.y);
-
-                setCamera(model);
-
-                /*POLAR GRID HELPER*/
-                var radius = 10;
-                var radials = 16;
-                var circles = 8;
-                var divisions = 64;
-
-                polar_grid_helper = new THREE.PolarGridHelper(bbox.max.x * 4, radials, circles, divisions);
-                polar_grid_helper.position.y = bbox.min.y;
-                polar_grid_helper.visible = false;
-                model.add(polar_grid_helper);
-
-                /*NORMAL GRID HELPER*/
-                gridHelper = new THREE.GridHelper(bbox.max.x * 4, 40, 0xe6e600, 0x808080);
-                //Set size of grid to cover objects of all sizes based on the non visible box3() size.
-                // gridHelper.position.x = 0;
-                gridHelper.position.y = bbox.min.y; //Set grid underneath loaded model object
-                // gridHelper.position.z = 0;
-                gridHelper.visible = false; //Grid visibility initially false, until grid checkbox is selected
-                model.add(gridHelper);
-
-                /*AXIS HELPER*/
-                axis_view = new THREE.AxisHelper(bbox.max.z * 10); //Set axis size based on the non visible box3() size.
-                axis_view.position.y = bbox.min.y; //Set axis underneath loaded model object
-                axis_view.visible = false; //axis visibility initially false, until axis checkbox is selected
-                model.add(axis_view);
+                setBoundBox(model);
+                setPolarGrid(model);
+                setGrid(model);
+                setAxis(model);
 
                 console.log(renderer.info.memory.geometries);
 
@@ -195,7 +74,6 @@ function readFile(evt) {
                 });
 
                 scene.add(model);
-                objects.push(model);
             };
             reader.readAsText(fileObject);
         }
