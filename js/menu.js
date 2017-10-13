af145d3b2be25ca8334f7f193caccc3010fe9c6c
Adam James <adjam1212@gmail.com>
2017-10-13 17:20:10 +0100

    Make Menus Collapsible

--- js/menu.js
+++ js/menu.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             // open new menu and add the open class
             $(this).next("ul").slideDown(350);
             $(this).addClass("active");
-            //$(".side_menu").niceScroll({ horizrailenabled: false }); //add jquery nicescroll with horizontal scroll disabled
+           // $(".side_menu").niceScroll({ horizrailenabled: false }); //add jquery nicescroll with horizontal scroll disabled
             
         } else if ($(this).hasClass("active")) {
 
@@ -38,4 +38,84 @@ $(document).ready(function () {
             $(this).next("ul").slideUp(350);
         }
     });
+
+    var side_collapsed = false;
+    var btm_collapsed = false;
+
+    //Collapse Side Menu
+    $('#collapse_side').on('click', function () {
+
+        side_collapsed = !side_collapsed;
+        var arrow = document.getElementById('collapse_side');
+
+        if (side_collapsed) {
+            $('.side_menu').toggle('slide', 'left', 1200);
+            renderer.setSize(window.innerWidth, window.innerHeight);
+
+            // move the left collapse arrow along with menu and set it to be a right arrow (bring back menu)
+            $('#collapse_side').css('position', 'absolute').animate({
+                left: '-=270'
+            }, {
+                duration: 1500,
+                complete: function () {
+                    arrow.innerHTML = "&rarr;";
+                }
+            });
+        }
+        else {
+            $('.side_menu').toggle('slide', 'left', 1200);
+            renderer.setSize(winDims[0], winDims[1]);
+
+            // move the right collapse back along with menu
+            $('#collapse_side').css('position', 'absolute').animate({
+                left: '+=270'
+            }, {
+                duration: 1100,
+                complete: function () {
+                    arrow.innerHTML = "&larr;";
+                }
+            });
+        }
+    });
+
+
+    //Collapse Bottom Menu
+    $('#collapse_btm').on('click', function () {
+
+        btm_collapsed = !btm_collapsed;
+        var arrow = document.getElementById('collapse_btm');
+
+        if (btm_collapsed) {
+            //$('#bottom_menu').toggle('slide', 'down', 1200);
+            $("#bottom_menu").slideToggle("slow");
+            renderer.setSize(window.innerWidth, window.innerHeight);
+
+            // move the left collapse arrow along with menu and set it to be a right arrow (bring back menu)
+            $('#collapse_btm').css('position', 'absolute').animate({
+                bottom: '-=270'
+            }, {
+                duration: 1500,
+                complete: function () {
+                    arrow.innerHTML = "&uarr;";
+                }
+            });
+
+        }
+        else {
+            //$('#bottom_menu').toggle('slide', 'down', 1200);
+            $("#bottom_menu").slideToggle("slow");
+            renderer.setSize(winDims[0], winDims[1]);
+
+            // move the right collapse back along with menu
+            $('#collapse_btm').css('position', 'absolute').animate({
+                top: '-=270'
+            }, {
+                duration: 600,
+                complete: function () {
+                    arrow.innerHTML = "&darr;";
+                }
+            });
+        }
+    });
+
 });
