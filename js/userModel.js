c248a1f11c816863213be278b0c704e7610cd369
Adam James <adjam1212@gmail.com>
2018-06-21 15:48:36 +0100

    Update userModel.js

--- js/userModel.js
+++ js/userModel.js
@@ -22,9 +22,18 @@ document.addEventListener('drop', function (event) {
     event.stopPropagation(); //Only call loadFile function on drop event (default is to display file as plain text file).
     event.preventDefault();
 
-    if (event.dataTransfer.files.length > 0) {
+    if (event.dataTransfer.files.length > 0 && event.dataTransfer.files.length < 2) {
 
+        //Just one file selected e.g. .obj, .dae, .stl
         loadFile(event.dataTransfer.files[0]);
+    }
+
+   //More than 1 file (minimum two for model plus texture)
+   else if (event.dataTransfer.files.length > 1) {
+
+        //Send to file manager in userModelTextures.js
+        var files = event.dataTransfer.files;
+        loadFiles(files);
     }
 
 }, false);
@@ -55,98 +64,78 @@ var loadFile = function (file) {
 
         case 'obj':
 
-                //When file type matches case - remove sample model or remove previously loaded model from user file
-                scene.remove(sample_model);
-                removeModel();
-                modelLoaded = true;
-                modelWithTexLoaded = false;
-                
-                reader.addEventListener( 'load', function ( event ) {
+            //When file type matches case - remove sample model or remove previously loaded model from user file
+            scene.remove(sample_model);
+            removeModel();
+            modelLoaded = true;
+            modelWithTexures = false;
 
-                var contents = event.target.result;
-               // model = loader.parse(contents);
+            reader.addEventListener( 'load', function ( event ) {
 
-                try {
-                    model = loader.parse(contents);
+            var contents = event.target.result;
+            // model = loader.parse(contents);
+
+            try {
+                model = loader.parse(contents);
+            }
+            catch (err) {
+                //Model fails to load due to parsing error
+                alert("Problem parsing file: " + filename + "\n\n" + "ERROR MESSAGE: " + err.message);
+            }
+
+            model.traverse(function (child) {
+                if (child instanceof THREE.Mesh) {
+
+                    numOfMeshes++;
+                    var geometry = child.geometry;
+                    stats(filename, geometry, numOfMeshes);
+
+                    child.material = materials.default_material;
+
+                    var wireframe2 = new THREE.WireframeGeometry(child.geometry);
+                    var edges = new THREE.LineSegments(wireframe2, materials.wireframeAndModel);
+                    materials.wireframeAndModel.visible = false;
+                    model.add(edges);
+
+                    setWireFrame(child);
+                    setWireframeAndModel(child);
+
+                    setPhong(child);
+                    setXray(child);
+
                 }
-                catch (err) {
-                    //Model fails to load due to parsing error
-                    alert("Problem parsing file: " + filename + "\n\n" + "ERROR MESSAGE: " + err.message);
-                }
-
-                var geometry;
-                model.traverse(function (child) {
-                    if (child instanceof THREE.Mesh) {
-
-                        numOfMeshes++;
-                        geometry = new THREE.Geometry().fromBufferGeometry(child.geometry);
-                        console.log(geometry);
-
-                        if (geometry !== undefined) {
-                         //Dislay file name, number of vertices and faces info of model
-                         statsNode.innerHTML = 'Name of model/file: ' + '<span class="statsText">' + filename + '</span>'
-                            + '<br>'
-                            + 'Number of vertices: ' + '<span class="statsText">' + geometry.vertices.length + '</span>'
-                            + '<br>'
-                            + 'Number of faces: ' + '<span class="statsText">' + geometry.faces.length + '</span>'
-                            + '<br>'
-                            + 'Number of Meshes: ' + '<span class="statsText">' + numOfMeshes + '</span>';
-                        }
-    
-                        child.material = materials.default_material;
-
-                        var wireframe2 = new THREE.WireframeGeometry(child.geometry);
-                        var edges = new THREE.LineSegments(wireframe2, materials.wireframeAndModel);
-                        materials.wireframeAndModel.visible = false;
-                        model.add(edges);
-
-                        setWireFrame(child);  //LARGE MODEL/SLOW LOADING - FINE
-                        setWireframeAndModel(child);
-
-                        setPhong(child);
-                        setXray(child);
-
-                    }
-                });
+            });
                 
-                setCamera(model);
+            setCamera(model);
 
-                setSmooth(model);
+            setSmooth(model);
 
-                model.position.set(0, 0, 0);
+            model.position.set(0, 0, 0);
 
-                setBoundBox(model);
-                setPolarGrid(model);
-                setGrid(model);
-                setAxis(model);
+            setBoundBox(model);
+            setPolarGrid(model);
+            setGrid(model);
+            setAxis(model);
 
-                scaleUp(model);
-                scaleDown(model);
+            scaleUp(model);
+            scaleDown(model);
 
-                console.log(renderer.info.memory.geometries);
+            console.log(renderer.info.memory.geometries);
 
-                //Some models may load in with incorrect rotation on X axis
-                fixRotation(model);
+            //Some models may load in with incorrect rotation on X axis
+            fixRotation(model);
+            resetRotation(model);
 
-                //Reset only runs when user model is loaded, as sample models all have correct orientation
-                $("#reset_rot").click(function () {
-                    model.rotation.set(0, 0, 0);
-                    polar_grid_helper.rotation.set(0, 0, 0);
-                    gridHelper.rotation.set(0, 0, 0);
-                    axis_view.rotation.set(0, 0, 0);
-                    $('input[name="rotate"]').prop('checked', false);
-                });
+            selectedObject = model;
+            outlinePass.selectedObjects = [selectedObject];
+            outlinePass.enabled = false;
 
-                selectedObject = model;
-                outlinePass.selectedObjects = [selectedObject];
-                outlinePass.enabled = false;
+            scene.add(model);             
 
-                scene.add(model);             
+        }, false );
+        reader.readAsText( file );
 
-            }, false );
-            reader.readAsText( file );
-
-            break;
+        break;
 
         case 'stl':
 
@@ -156,7 +145,7 @@ var loadFile = function (file) {
                 scene.remove(sample_model);
                 removeModel();
                 modelLoaded = true;
-
+                
                 var contents = event.target.result;
               
                 try {
@@ -169,7 +158,6 @@ var loadFile = function (file) {
                 }
 
                 model = new THREE.Mesh(geometry, materials.default_material);
-
                 console.log(renderer.info.memory.geometries);
 
                 model.traverse(function (child) {
@@ -177,20 +165,8 @@ var loadFile = function (file) {
                     if (child instanceof THREE.Mesh) {
 
                         numOfMeshes++;
-                        //Convert to normal geometry in order to apply computation of vertex normals and face normals
-                        //and to merge vertices.
-                        var geometry2 = new THREE.Geometry().fromBufferGeometry(child.geometry);
-                   
-                        if (geometry2 !== undefined) {
-                            //Dislay file name, number of vertices and faces info of model
-                            statsNode.innerHTML = 'Name of model/file: ' + '<span class="statsText">' + filename + '</span>'
-                                + '<br>'
-                                + 'Number of vertices: ' + '<span class="statsText">' + geometry2.vertices.length + '</span>'
-                                + '<br>'
-                                + 'Number of faces: ' + '<span class="statsText">' + geometry2.faces.length + '</span>'
-                                + '<br>'
-                                + 'Number of Meshes: ' + '<span class="statsText">' + numOfMeshes + '</span>';
-                        }
+                        var geometry = child.geometry;
+                        stats(filename, geometry, numOfMeshes);
 
                         child.material = materials.default_material;
 
@@ -222,23 +198,15 @@ var loadFile = function (file) {
                 scaleDown(model);
 
                 fixRotation(model);
-                //fixRotation(glowModel);
-
-                //Reset only runs when user model is loaded, as sample models all have correct orientation
-                $("#reset_rot").click(function () {
-                    model.rotation.set(0, 0, 0);
-                   // glowModel.rotation.set(0, 0, 0);
-                    polar_grid_helper.rotation.set(0, 0, 0);
-                    gridHelper.rotation.set(0, 0, 0);
-                    axis_view.rotation.set(0, 0, 0);
-                    $('input[name="rotate"]').prop('checked', false);
-                });
+                resetRotation(model);
 
                 selectedObject = model;
                 outlinePass.selectedObjects = [selectedObject];
                 outlinePass.enabled = false;
 
                 scene.add(model);
+
+                objects.push(model);
 
             }, false);
 
@@ -254,7 +222,7 @@ var loadFile = function (file) {
             break;
 
         case 'dae':
-
+     
             var onError = function (xhr) {
                 console.log('ERROR');
             };
@@ -269,7 +237,6 @@ var loadFile = function (file) {
                 var contents = event.target.result;
 
                 var loader = new THREE.ColladaLoader();
-                //loader.options.convertUpAxis = true;
 
                 try {
                     var collada = loader.parse(contents);
@@ -279,112 +246,10 @@ var loadFile = function (file) {
                     alert("Problem parsing file: " + filename + "\n\n" + "ERROR MESSAGE: " + err.message);
                 }
 
-                collada.scene.name = filename;
                 var dae = collada.scene;
-
                 console.log(dae);
-
-                dae.traverse(function (child) {
-
-                    if (child instanceof THREE.Mesh) {
-
-                        var geometry2 = new THREE.Geometry().fromBufferGeometry(child.geometry);
-
-                        if (geometry2 !== undefined) {
-                            //Dislay file name, number of vertices and faces info of model
-                            statsNode.innerHTML = 'Name of model/file: ' + '<span class="statsText">' + filename + '</span>'
-                                + '<br>'
-                                + 'Number of vertices: ' + '<span class="statsText">' + geometry2.vertices.length + '</span>'
-                                + '<br>'
-                                + 'Number of faces: ' + '<span class="statsText">' + geometry2.faces.length + '</span>';
-                        }
-
-                        console.log(child.geometry); //Original ColladaLoader uses normal geometry, 
-                                                    //ColladaLoader2 uses BufferGeometry
-
-                        //child.geometry = new THREE.BufferGeometry().fromGeometry(child.geometry);
-                        var wireframe2;
-                        var collada_geometry = new THREE.Geometry();
-
-                        if (dae.children.length > 1) {
-                            for (var i = 0; i < dae.children.length; i++) {
-                                var col_child = dae.children[i];
-
-                                console.log(dae.children.length);
-
-                                if (col_child instanceof THREE.Mesh) {
-
-                                    //When there is more than one child BufferGeometry, create a new geometry 
-                                    //and merge it with the main new geometry (defined above as collada_geometry)
-                                    var geom = new THREE.Geometry().fromBufferGeometry(col_child.geometry);
-                                    collada_geometry.merge(geom);
-                                    console.log(collada_geometry);
-
-                                  //  var buffer_collada_geometry = new THREE.BufferGeometry().fromGeometry(collada_geometry);
-                                   // console.log(buffer_collada_geometry);
-                                }
-                            }
-
-                            model = new THREE.Mesh(collada_geometry, materials.default_material);
-                            wireframe2 = new THREE.WireframeGeometry(collada_geometry);
-                            glowModel = new THREE.Mesh(collada_geometry, materials.glowMaterial);
-
-
-                            $('#smooth').change(function () {
-                                
-                                if (smooth.checked) {
-                                    collada_geometry.mergeVertices();
-                                    collada_geometry.computeVertexNormals();
-                                    collada_geometry.computeFaceNormals();
-                                }
-                                else {
-                                    collada_geometry.computeFlatVertexNormals();
-                                }
-                            });
-
-                            console.log("More than one Geometry");
-                        }
-                        else {
-                            var single_geom = new THREE.Geometry().fromBufferGeometry(child.geometry);
-
-                            model = new THREE.Mesh(single_geom, materials.default_material);
-                            wireframe2 = new THREE.WireframeGeometry(single_geom);
-                            glowModel = new THREE.Mesh(single_geom, materials.glowMaterial);
-
-                            console.log("One Geometry");
-
-                            $('#smooth').change(function () {
-                               
-                                if (smooth.checked) {
-                                    single_geom.mergeVertices();
-                                    single_geom.computeVertexNormals();
-                                    single_geom.computeFaceNormals();
-                                    child.geometry = new THREE.BufferGeometry().fromGeometry(single_geom);
-                                }
-                                else {
-                                    single_geom.computeFlatVertexNormals();
-                                    child.geometry = new THREE.BufferGeometry().fromGeometry(single_geom);
-                                }
-                            });
-                           
-                        }
-
-                        child.material = materials.default_material;
-
-                        var edges = new THREE.LineSegments(wireframe2, materials.wireframeAndModel);
-                        materials.wireframeAndModel.visible = false;
-                        model.add(edges);
-
-                        setWireFrame(model);
-                        setWireframeAndModel(model);
-
-                        setPhong(model);
-                        setXray(model);
-                    }
-
-                });
-
-              //  model.position.set(0, 0, 0);
+                
+                colladaMerge(dae, filename);
 
                 setCamera(model);
                 setBoundBox(model);
@@ -396,16 +261,7 @@ var loadFile = function (file) {
                 scaleDown(model);
 
                 fixRotation(model);
-
-                //Reset only runs when user model is loaded, as sample models all have correct orientation
-                $("#reset_rot").click(function () {
-                    model.rotation.set(0, 0, 0);
-                    // glowModel.rotation.set(0, 0, 0);
-                    polar_grid_helper.rotation.set(0, 0, 0);
-                    gridHelper.rotation.set(0, 0, 0);
-                    axis_view.rotation.set(0, 0, 0);
-                    $('input[name="rotate"]').prop('checked', false);
-                });
+                resetRotation(model);
 
                 selectedObject = model;
                 outlinePass.selectedObjects = [selectedObject];
