799ec3f6c0c5636fec55d7714e7112a1cf405a7c
Adam James <adjam1212@gmail.com>
2018-06-21 16:28:37 +0100

    Update userModel.js

--- js/userModel.js
+++ js/userModel.js
@@ -206,8 +206,6 @@ var loadFile = function (file) {
 
                 scene.add(model);
 
-                objects.push(model);
-
             }, false);
 
             if (reader.readAsBinaryString !== undefined) {
